1695ae5d18e719efc29dc16d3a39c61bc5190fd3
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-08-21 23:46:44 +0100

    Remove verbose flag from HTTP service and command options
    This change refactors the HTTP service to always log at the verbose level, removing the need for a flag. Additionally, the verbose option is removed from the base command to simplify configuration. Logging levels are now dynamically set based on the configuration in the main bootstrap function.

--- src/commands/base/base.command.ts
+++ src/commands/base/base.command.ts
@@ -1,11 +1,3 @@
-import {CommandRunner, Option} from 'nest-commander';
+import {CommandRunner} from 'nest-commander';
 
-export abstract class BaseCommand extends CommandRunner {
-  @Option({
-    flags: '-v, --verbose [boolean]',
-    description: 'Whether or not to enable verbose logging',
-  })
-  parseVerbose(val: boolean): boolean {
-    return val;
-  }
-}
+export abstract class BaseCommand extends CommandRunner {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
+import {LogLevel} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
+import {NestFactory} from '@nestjs/core';
 import {CommandFactory} from 'nest-commander';
 
 import {AppModule} from './app.module';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+  let loggerLevels: LogLevel[] = ['log', 'warn', 'error', 'fatal'];
+  if (configService.get('VERBOSE')) {
+    loggerLevels = [...loggerLevels, 'debug', 'verbose'];
+  }
   await CommandFactory.run(AppModule, {
-    logger: ['log', 'warn', 'error', 'fatal'],
+    logger: loggerLevels,
   });
 }
 bootstrap().catch((reason) => console.error(reason));
