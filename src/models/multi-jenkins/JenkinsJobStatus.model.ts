704c28c9a7c52f6e3141b0d94d7125460229de9a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-08-24 22:20:50 +0100

    Add Jenkins job status to tracking model
    Created a new `JenkinsJobStatus` type to define job statuses.
    Updated the `TrackJob` model to include `buildIndex` and `status` fields.

---
+++ src/models/multi-jenkins/JenkinsJobStatus.model.ts
@@ -0,0 +1,1 @@
+export type JenkinsJobStatus = 'UNKNOWN' | 'PENDING' | 'SUCCESS' | 'FAILURE';
--- src/models/multi-jenkins/tracking/TrackJob.model.ts
+++ src/models/multi-jenkins/tracking/TrackJob.model.ts
@@ -1,6 +1,9 @@
+import {JenkinsJobStatus} from '../JenkinsJobStatus.model';
 import {Parameter} from '../Parameter.model';
 
 export interface TrackJob {
   path: string;
+  buildIndex: number;
   buildParameters: Parameter[];
+  status: JenkinsJobStatus;
 }
