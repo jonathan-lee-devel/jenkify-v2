f67ce14a857b5b5bb6e74c8ef4a87b9f1d4cf52d
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-01-09 04:11:15 +0000

    Remove multi-Jenkins start and track build commands
    Deleted multi-Jenkins `start-builds` and `track-builds` commands, related YAML configuration files, and their associated test files. This cleanup also includes updates to `app.module.ts` to unregister these commands.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import {ConfigModule} from '@nestjs/config';
 
 import {StartBuildCommand} from './commands/jenkins/start-build.command';
 import {CompleteBuildsCommand} from './commands/multi-jenkins/complete-builds.command';
-import {StartBuildsCommand} from './commands/multi-jenkins/start-builds.command';
-import {TrackBuildsCommand} from './commands/multi-jenkins/track-builds.command';
 import {HttpConfigService} from './config/http-config/http-config.service';
 import {JenkinsRestApiService} from './services/jenkins-rest-api/jenkins-rest-api.service';
 import {JenkinsWfApiService} from './services/jenkins-wf-api/jenkins-wf-api.service';
@@ -28,8 +26,6 @@ import {YamlParserService} from './services/yaml-parser/yaml-parser.service';
     },
     HttpConfigService,
     StartBuildCommand,
-    StartBuildsCommand,
-    TrackBuildsCommand,
     CompleteBuildsCommand,
     JenkinsRestApiService,
     JenkinsWfApiService,
